71f18f6001fa1c832d91ccdbdeb60ef713832ca3
Alvaro J Torres <alvarojtorres23@hotmail.com>
2024-03-15 22:23:57 -0500

    feat(): ajuste gitignore y eliminacion de timeout

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const { Builder, Browser, By, Key } = require('selenium-webdriver');
 const ExcelJS = require('exceljs');
 
-let sleep = (ms) => {
+const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 const trabajo = async () => {
   let result = [['Nombre del Libro', 'Autores', 'Citados Por', 'URL']];
   let navegador = await new Builder().forBrowser(Browser.CHROME).build();
-  await sleep(1000);
+//   await sleep(1000);
   try {
     await navegador.get('https://scholar.google.com/');
     await navegador
       .findElement(By.name('q'))
       .sendKeys('software testing', Key.RETURN);
-    await sleep(1000);
+    await sleep(6000);
 
     const enlaces = await navegador.findElements(
       By.css('.gs_r, .gs_or, .gs_scl')
     );
-    await sleep(1000);
+    // await sleep(1000);
     for (let enlace of enlaces) {
       nombres = await enlace.findElements(By.css('.gs_rt a'));
       autores = await enlace.findElements(By.css('.gs_a'));
@@ -28,11 +28,11 @@ const trabajo = async () => {
       const { direccion, nombre } = await buscarNombres(nombres);
       const { autor } = await buscarAutor(autores);
       const { citadoPor } = await buscarCitas(citadosPor);
-    //   console.log({nombre, autor, citadoPor, direccion});
+      //   console.log({nombre, autor, citadoPor, direccion});
       if (direccion && nombre) {
         result.push([nombre, autor, citadoPor, direccion]);
       }
-      await sleep(10);
+    //   await sleep(10);
     }
   } catch (ex) {
     console.error('Error encontrado', ex);
@@ -48,7 +48,7 @@ const buscarNombres = async (enlaces) => {
   for (let enlace of enlaces) {
     let direccion = await enlace.getAttribute('href');
     let texto = await enlace.getAttribute('text');
-    await sleep(50);
+    // await sleep(50);
     result = { direccion, nombre: texto };
   }
   return result;
@@ -57,7 +57,7 @@ const buscarAutor = async (enlaces) => {
   let result = {};
   for (let enlace of enlaces) {
     let autor = await enlace.getText();
-    await sleep(50);
+    // await sleep(50);
     result = { autor };
   }
   return result;
@@ -70,7 +70,7 @@ const buscarCitas = async (enlaces) => {
       By.xpath('//a[contains(text(), "Citado por")]')
     );
     let texto = await citadoPor.getText();
-    await sleep(50);
+    // await sleep(50);
     result = { citadoPor: texto };
   }
   return result;
